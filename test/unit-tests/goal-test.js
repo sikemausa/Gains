d742782ef57f7edead5c029bcfaaaa6586ccbda5
Madison Kerndt <madison.kerndt@gmail.com>
2016-09-05 15:37:32 -0600

    format html

--- test/unit-tests/goal-test.js
+++ test/unit-tests/goal-test.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD:test/index.js
-'use strict';
-const assert = require('chai').assert;
-=======
 const assert = require('assert');
->>>>>>> c2c4eb14da4d995ae8b8ccffce7a5a71db90c7e1:test/unit-tests/goal-test.js
 
 describe('our test bundle', function() {
   it('should work', function() {
