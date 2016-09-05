981c4cf668ce89bec3299c5428473e8b6f54a79d
Mike Sausa <mikejsausa@gmail.com>
2016-09-05 10:29:12 -0600

    webdriver.io set up

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 'use strict';
-const assert = require('chai').assert;
+// const assert = require('chai').assert;
+const assert = require('assert');
 
 describe('our test bundle', function() {
   it('should work', function() {
