f52c2517c8478db9278d1bea3243da92deb1f6cb
Madison Kerndt <madison.kerndt@gmail.com>
2016-09-05 16:19:20 -0600

    WIP action methods

--- test/unit-tests/action-test.js
+++ test/unit-tests/action-test.js
@@ -1,7 +1,44 @@
-const assert = require('assert');
+'use strict';
+// const assert = require('assert');
+const assert = require('chai').assert;
+const Action = require('../../lib/objects/action');
 
 describe('our test bundle', function() {
   it('should work', function() {
     assert(true);
+  });
+
+  it('should be a function', function() {
+    assert.isFunction(Action);
+  });
+});
+
+describe('Action', function() {
+
+    beforeEach(function() {
+      this.rightNow = Date.now();
+      this.dateNow = Date.now;
+      Date.now = () => this.rightNow;
+    });
+
+    afterEach(function() {
+      Date.now = this.dateNow;
+    });
+
+  context('action constructor', function(){
+    let action = new Action();
+
+    it('should return .startTime if .start is called', function() {
+      // this is not functioning properly
+      let time = Date.now();
+      action.start;
+      assert.equal(action.startTime, action.start);
+    });
+
+    it('should return .scheduled if the scheduled method is called', function() {
+      let duration = 20;
+      action.scheduled;
+      assert.equal(action.scheduledTime, action.scheduled);
+    });
   });
 });
